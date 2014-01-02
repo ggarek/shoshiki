e6abe834ecd4f4181ceb37091cc514e959eec1d8
igor.ovsyannikov <igor.ovsyannikov@altspace.com>
2014-01-02 20:24:58 +0400

    custom number format algorithm updated

--- test/numbers.js
+++ test/numbers.js
@@ -1,8 +1,12 @@
+/**
+ * Thoughts:
+ * - May be use some 'resource' file with test data..
+ */
 var assert = require('assert');
 require('../lib/string-format');
 
 describe('String.format::numbers', function () {
-  describe('', function () {
+  describe('#custom number format', function () {
     it('', function () {
       var data = [
         {
@@ -36,9 +40,9 @@ describe('String.format::numbers', function () {
           formatted   : '0123 456 0789 000123'
         },
         {
-          input       : '{0:^#} {1:#000} {2:000#} {3:0##000}',
+          input       : '{0:^#} {1:^#^} {2:#^#*} {3:0##-00-^*^-}',
           params      : [123, 456, 789, 123],
-          formatted   : '^123 456 0789 000123'
+          formatted   : '^123 ^456^ 78^9* 001-23-^*^-'
         },
         {
           input       : '{0:#-#-#} {1:##-#-#} {2:0#-#-##} {3:#-#-000}',
@@ -49,6 +53,11 @@ describe('String.format::numbers', function () {
           input       : '{0:0.00} {1:0000.##0} {2:####.0.0.0} {3:#-#-#.###00.0} {4:000.00}',
           params      : [123.56, 456.12, 789.123, 123.456, 456.789],
           formatted   : '123.56 0456.120 789.123 1-2-3.456000 456.78'
+        },
+        {
+          input       : '{0:0.#-#} {1:0000.#*#*0} {2:####.0.-0.-0} {3:#^#.#-#---} {4:#-#.0-0-0-0}',
+          params      : [123.56, 456.12, 789.123, 123.456, 456.789],
+          formatted   : '123.5-6 0456.1*2*0 789.1-2-3 12^3.4-5--- 45-6.7-8-9-0'
         }
       ];
       
